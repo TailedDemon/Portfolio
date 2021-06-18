33fb17e754458577e7407e993629d0f83cb4d713
Niikotey Amon <70966958+TailedDemon@users.noreply.github.com>
2021-06-18 13:07:19 +0100

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
     $('.top-nav').toggleClass('open');
   });
 
-  // $('.top-nav .nav-link').on('click', function () {
-  //   $('.menu-toggler').removeClass('open');
-  //   $('.top-nav').removeClass('open');
-  // });
+  $('.top-nav .nav-link').on('click', function () {
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+  });
 
   $('nav a[href*="#"]').on('click', function () {
     $('html, body').animate({
